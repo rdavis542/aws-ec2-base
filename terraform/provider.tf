950cf29d1f16bafd8f41fc3879286afa124d8464
Ryan Davis <rdavis542@gmail.com>
2026-05-24 16:34:11 -0400

    update for state file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,9 +7,9 @@ terraform {
     }
   }
   backend "s3" {
-    bucket  = "tfstategit"
+    bucket  = "tf-state-replication-source-350726165848"
     key     = "ec2-base-terraform.tfstate"
-    region  = "us-east-1"
+    region  = "us-east-2"
     encrypt = true
   }
 }
